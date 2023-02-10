67e27f8d5680236ee2f8e954793c35aa1889567c
Joao Lino <linusmannuel@gmail.com>
2023-02-10 12:53:55 +0100

    first commit make class cliente

---
+++ main.js
@@ -0,0 +1,15 @@
+class Cliente {
+	nome;
+	cpf;
+	agencia;
+	saldo;
+}
+
+const cliente1 = new Cliente();
+
+cliente1.nome = 'Linus';
+cliente1.cpf = 1234;
+cliente1.agencia = 'Cacuaco';
+cliente1.saldo = 10000;
+
+console.log(cliente1);
