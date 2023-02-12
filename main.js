680376df2223379dadda414cab878e44af2aca56
Joao Lino <linusmannuel@gmail.com>
2023-02-12 11:47:38 +0100

    create new class conta corrente

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 class Cliente {
 	nome;
 	cpf;
+}
+
+class ContaCorrente {
 	agencia;
 	saldo;
 }
 
 const cliente1 = new Cliente();
-
 cliente1.nome = 'Linus';
 cliente1.cpf = 1234;
-cliente1.agencia = 'Cacuaco';
-cliente1.saldo = 10000;
-
 console.log(cliente1);
+
+const cliente2 = new Cliente();
+cliente2.nome = 'Molina';
+cliente2.cpf = 1999;
+
+const contaCorrenteLinus = new ContaCorrente();
+contaCorrenteLinus.agencia = 1001;
+contaCorrenteLinus.saldo = 0;
+console.log(contaCorrenteLinus.saldo);
+contaCorrenteLinus.saldo += 100;
+console.log(contaCorrenteLinus.saldo);
+let saque = 100;
+
+if (contaCorrenteLinus.saldo >= saque) {
+	contaCorrenteLinus.saldo -= saque;
+}
+
+console.log(contaCorrenteLinus.saldo);
