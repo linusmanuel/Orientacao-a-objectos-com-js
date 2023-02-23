33147f7a36cde044e410b5b89b6fdf55845a24a3
Joao Lino <linusmannuel@gmail.com>
2023-02-23 07:09:39 +0100

    export class conta corrente

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-class ContaCorrente {
-	_agencia;
-	_saldo = 0;
-
-	sacar(valor) {
-		if (this._saldo >= valor) {
-			this._saldo -= valor;
-			return valor;
-		}
-	}
-
-	depositar(valor) {
-		if (valor < 0) return;
-		this._saldo += valor;
-	}
-}
-
 const contaCorrenteLinus = new ContaCorrente();
 contaCorrenteLinus._agencia = 1001;
 contaCorrenteLinus.depositar(150);
